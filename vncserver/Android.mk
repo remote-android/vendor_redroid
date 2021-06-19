a21f52e3e887c6f5c9cbfc2b5ad5deac666afd2c
Ziyang Zhou <ziyang.zhou@outlook.com>
2021-06-19 22:21:00 +0800

    init for redroid-12.0.0

---
+++ vncserver/Android.mk
@@ -0,0 +1,55 @@
+# Copyright 2013 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+	main.cpp \
+	EglWindow.cpp \
+	FrameOutput.cpp \
+	Program.cpp \
+	InputManager.cpp \
+	InputDevice.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+	libutils libbinder libgui libEGL libGLESv2 \
+	libz libcrypto libssl libjpeg libpng libcutils liblog libinput
+
+PLATFORM_VERSION_MAJOR := $(word 1, $(subst ., ,$(PLATFORM_VERSION)))
+
+ifneq ($(filter $(PLATFORM_VERSION_MAJOR), 9 10 11),)
+# Android 9 and above
+LOCAL_SHARED_LIBRARIES += libui
+endif
+
+LOCAL_C_INCLUDES := external/libvncserver \
+					frameworks/native/include
+
+LOCAL_STATIC_LIBRARIES += libvncserver
+
+LOCAL_INIT_RC := vncserver.rc
+
+LOCAL_CFLAGS := -Werror -Wall
+LOCAL_CFLAGS += -Wno-multichar
+#LOCAL_CFLAGS += -UNDEBUG
+LOCAL_CFLAGS += -DPLATFORM_VERSION=$(PLATFORM_VERSION_MAJOR)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_VENDOR_MODULE = true
+LOCAL_MODULE:= vncserver
+
+include $(BUILD_EXECUTABLE)
