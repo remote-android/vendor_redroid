c3390a56221a2f38e6017138c4d56b0fa198c285
Ziyang Zhou <ziyang.zhou@outlook.com>
2022-06-04 11:27:21 +0800

    move common config from device/redroid

---
+++ vendor.mk
@@ -0,0 +1,3 @@
+PRODUCT_COPY_FILES += \
+    vendor/redroid/gpu_config.sh:$(TARGET_COPY_OUT_VENDOR)/bin/gpu_config.sh \
+    vendor/redroid/redroid.common.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/redroid.common.rc \
