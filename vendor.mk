1a5e8899cc60c370e7d593e569959d722b44354c
Ziyang Zhou <ziyang.zhou@outlook.com>
2022-06-11 10:47:12 +0800

    move vncserver to /external

--- vendor.mk
+++ vendor.mk
@@ -1,3 +1,10 @@
+PRODUCT_PACKAGES += \
+	binder_alloc \
+	hwcomposer.redroid \
+	gralloc.redroid \
+	ipconfigstore \
+
+
 PRODUCT_COPY_FILES += \
     vendor/redroid/gpu_config.sh:$(TARGET_COPY_OUT_VENDOR)/bin/gpu_config.sh \
     vendor/redroid/redroid.common.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/redroid.common.rc \
