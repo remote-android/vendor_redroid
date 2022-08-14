d4a21a49cb839d93ec44321a2b4a05f13259f716
Ziyang Zhou <ziyang.zhou@outlook.com>
2022-08-14 11:11:12 +0800

    add redroid.legacy.rc to handle legacy props

--- vendor.mk
+++ vendor.mk
@@ -9,3 +9,4 @@ PRODUCT_COPY_FILES += \
     vendor/redroid/gpu_config.sh:$(TARGET_COPY_OUT_VENDOR)/bin/gpu_config.sh \
     vendor/redroid/post-fs-data.redroid.sh:$(TARGET_COPY_OUT_VENDOR)/bin/post-fs-data.redroid.sh \
     vendor/redroid/redroid.common.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/redroid.common.rc \
+    vendor/redroid/redroid.legacy.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/redroid.legacy.rc \
